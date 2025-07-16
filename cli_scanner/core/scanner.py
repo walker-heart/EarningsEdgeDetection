5b098bfd4c91a679a055917854c9ba4bbe37e194
jayesh chhabra <chhabjay@amazon.com>
2025-07-16 07:24:03 -0700

    bugfix for mc winrate

--- cli_scanner/core/scanner.py
+++ cli_scanner/core/scanner.py
@@ -664,6 +664,7 @@ class EarningsScanner:
             self._driver = None
         
         options = webdriver.ChromeOptions()
+        options.add_argument("--window-size=1920,1080")
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
